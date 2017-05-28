487ad0fa25084f6ee6e1b84da7a77085fbed6f39
Thiago Brandão <thiagobrandam@gmail.com>
2017-05-28 10:41:31 -0300

    Adds vis and terms model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,14 +4,15 @@ class MessagesController < ApplicationController
 
   def transcript
     user_name = params['user']
-    content = params['message']
+    content   = params['message']
     timestamp = params['timestamp']
 
-    user = User.find_or_create_by(name: user_name)
+    user    = User.find_or_create_by(name: user_name)
     message = Message.create(user: user, content: content)
 
     ActionCable.server.broadcast 'messages',
       message: message.content,
+      nodes: Term.top_five,
       user: user_name,
       timestamp: timestamp
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528002045) do
+ActiveRecord::Schema.define(version: 20170528125444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 20170528002045) do
     t.integer  "room_session_id", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "terms", force: :cascade do |t|
+    t.string  "value"
+    t.integer "message_id"
+    t.index ["message_id"], name: "index_terms_on_message_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
