9dad0176d19f35a0880f337b96c53cf294a936de
Gustavo Guimarães <gustavoguimaraes18@gmail.com>
2017-05-28 03:22:34 -0300

    Response to user submit

--- app/assets/javascripts/room_session.js
+++ app/assets/javascripts/room_session.js
@@ -2,40 +2,49 @@
 
 var app = {
   setCookie: function(cname, cvalue, exdays) {
-      var d = new Date();
-      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-      var expires = "expires="+d.toUTCString();
-      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   },
 
   getCookie: function(cname) {
-      var name = cname + "=";
-      var ca = document.cookie.split(';');
-      for(var i = 0; i < ca.length; i++) {
-          var c = ca[i];
-          while (c.charAt(0) == ' ') {
-              c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-              return c.substring(name.length, c.length);
-          }
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
       }
-      return "";
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+    }
+    return "";
   },
 
   checkCookie: function() {
-      var user = this.getCookie("av_username");
-      if (user != "") {
-          alert("Welcome again " + user);
-      } else {
-          $('#modal').modal('open');
-          console.log('asdf');
-          //user = prompt("Please enter your name:", "");
-          if (user != "" && user != null) {
-              this.setCookie("av_username", user, 365);
-          }
-      }
+    var user = this.getCookie("av_username");
+    if (user == "") {
+      $('#modal').modal('open');
+    }
+    else {
+      console.log(user + " entrou")
+    }
   }
+}
+
+function submitUserForm() {
+  $.ajax({
+    url: $('#user-name-form').prop('action'),
+    type: $('#user-name-form').prop('method'),
+    data: $('#user-name-form').serialize(),
+    success: function(data) {
+      app.setCookie("av_username", data.user_name, 2);
+      $('#modal').modal('close');
+      console.log(data.user_name + " entrou")
+    }
+  });
 }
 
 $(document).ready(function(){
--- app/controllers/room_session_controller.rb
+++ app/controllers/room_session_controller.rb
@@ -11,7 +11,7 @@ class RoomSessionController < ApplicationController
   def set_user
     user = User.where(name: params['username']).first_or_create
     room_sessions_user = RoomSessionsUser.create(user_id: user.id, room_session_id: params['room_session_id'])
-    binding.pry
+
     respond_to do |format|
       format.json {
         render :json => {
